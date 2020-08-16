913f8b10535d6c7b839bc3413ae576e3d7c42953
hb-py <878403168@qq.com>
2020-08-16 21:49:39 +0800

    用户管理页完成

--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="manage">
+    <el-dialog :title="operateType === 'add' ? '新增用户' : '更新用户'" :visible.sync="isShow">
+      <CommonForm :formLabel="operateFormLabel" :form="operateForm"> </CommonForm>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="isShow = false">取 消</el-button>
+        <el-button type="primary" @click="confirm">确 定</el-button>
+      </span>
+    </el-dialog>
     <div class="manage_header">
-      <CommonForm></CommonForm>
+      <el-button type="primary" @click="addUser">+ 新增</el-button>
+      <CommonForm inline :formLabel="formLabel" :form="searchFrom">
+        <el-button type="primary" @click="serchUser">搜索</el-button>
+      </CommonForm>
     </div>
-    <div class="manage_content">
-      <CommonTable></CommonTable>
-    </div>
+    <CommonTable :tableData="tableData" :tableLabel="tableLabel" :config="config" @changePage="getList" @edit="editUser" @del="delUser"></CommonTable>
   </div>
 </template>
 
@@ -16,6 +24,182 @@ export default {
   components: {
     CommonForm,
     CommonTable
+  },
+  data() {
+    return {
+      isShow: false,
+      operateForm: {
+        name: '',
+        addr: '',
+        age: '',
+        birth: '',
+        sex: ''
+      },
+      // dialog  formlabel
+      operateFormLabel: [
+        {
+          model: 'name',
+          label: '姓名'
+        },
+        {
+          model: 'age',
+          label: '年龄'
+        },
+        {
+          model: 'sex',
+          label: '性别',
+          type: 'select',
+          opts: [
+            {
+              label: '男',
+              value: 1
+            },
+            {
+              label: '女',
+              value: 0
+            }
+          ]
+        },
+        {
+          model: 'birth',
+          label: '出生日期',
+          type: 'date'
+        },
+        {
+          model: 'addr',
+          label: '地址'
+        }
+      ],
+      operateType: 'add',
+      tableData: [],
+      tableLabel: [
+        {
+          prop: 'name',
+          label: '姓名'
+        },
+        {
+          prop: 'age',
+          label: '年龄'
+        },
+        {
+          prop: 'sexLabel',
+          label: '性别'
+        },
+        {
+          prop: 'birth',
+          label: '出生日期',
+          width: 200
+        },
+        {
+          prop: 'addr',
+          label: '地址',
+          width: 320
+        }
+      ],
+      config: {
+        page: 1,
+        total: 30,
+        loading: false
+      },
+      searchFrom: {
+        keyword: ''
+      },
+      formLabel: [
+        {
+          model: 'keyword',
+          label: '',
+          options: []
+        }
+      ]
+    }
+  },
+  methods: {
+    getList() {
+      ;(this.config.loading = true),
+        this.$http
+          .get('/api/user/getUser', {
+            params: {
+              page: this.config.page
+            }
+          })
+          .then(res => {
+            this.tableData = res.data.list.map(item => {
+              item.sexLabel = item.sex === 0 ? '女' : '男'
+              return item
+            })
+            this.config.total = res.data.count
+            this.config.loading = false
+          })
+    },
+    editUser(row) {
+      this.operateType = 'edit'
+      this.isShow = true
+      this.operateForm = row
+    },
+    delUser(row) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          let id = row.id
+          this.$http
+            .get('/api/user/del', {
+              params: {
+                id
+              }
+            })
+            .then(res => {
+              console.log(res)
+              this.$message({
+                type: 'success',
+                message: '删除成功'
+              })
+              this.getList()
+            })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    addUser() {
+      this.operateForm = {}
+      this.operateType = 'add'
+      this.isShow = true
+    },
+    serchUser() {
+      this.$http
+        .get('/api/user/getUser', {
+          params: {
+            name: this.config.name
+          }
+        })
+        .then(res => {
+          console.log(res.data.list[1].name)
+        })
+    },
+    confirm() {
+      if (this.operateType === 'edit') {
+        this.$http.post('/api/user/edit', this.operateForm).then(res => {
+          console.log(res)
+          this.isShow = false
+          this.getList()
+        })
+      } else if (this.operateType === 'add') {
+        this.$http.post('/api/user/add', this.operateForm).then(res => {
+          console.log(res)
+          this.isShow = false
+          this.getList()
+        })
+      }
+    }
+  },
+  created() {
+    this.getList()
   }
 }
 </script>
